8894bdd9da208b09ce1038a0081a6c38b2884782
Brighton-Gold <113638673+Brighton-Gold@users.noreply.github.com>
2022-10-18 17:24:51 -0600

    display update

--- Program.cs
+++ Program.cs
@@ -3,7 +3,13 @@ class Program
 
     static void Main(string[] args)
     {
+        /// Test for display blank list. Works!
+        Display display = new Display();
+        string[] BlankList = {"_","_","_","_"};
+        display.DisplayJumper(false, BlankList);
+
         
+
     }
 
 }
--- display.cs
+++ display.cs
@@ -1,6 +1,33 @@
-Class Display 
+class Display
 {
+    string[] Jumper = { " ---", @"/___\", @"\   /", @" \ /" };
+
+    public void DisplayJumper(bool IsGuessTrue, string[] BlankList)
+    {
+        foreach (var i in BlankList)
+        {
+            Console.Write(i);
+            Console.Write("  ");
+
+        }
 
 
-    
+        if (IsGuessTrue == false)
+        {
+            Jumper.RemoveAt(0); //need to fix
+        }
+
+        foreach (var i in Jumper)
+        {
+            Console.WriteLine(i);
+
+        }
+
+
+
+
+
+    }
+
+
 }
