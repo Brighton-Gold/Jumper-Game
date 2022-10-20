c92809614a3f1ee758fb4d4fd753e2ed6bfd8b92
Brighton-Gold <113638673+Brighton-Gold@users.noreply.github.com>
2022-10-20 14:16:30 -0600

    Added CompareWord

---
+++ CompareWordtoGuess.cs
@@ -0,0 +1,12 @@
+class CompareWord{
+
+    private string CompareLetter(string Answer, string Guess){
+
+        GuessIsTrue = false;
+        foreach(string letter in Answer)
+        {
+            if(letter == Guess)
+
+        }
+    }
+}
